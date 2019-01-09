9f5903e5e2df732dd14baf16da034b42c73fe012
Filip Hanik <fhanik@pivotal.io>
2019-01-08 17:07:44 -0800

    Show how to spy on a failure handler to verify the exception

--- samples/bearer-token-filter-chain/src/main/java/org/springframework/security/community/samples/SecurityConfig.java
+++ samples/bearer-token-filter-chain/src/main/java/org/springframework/security/community/samples/SecurityConfig.java
@@ -6,10 +6,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	AuthenticationFailureHandler failureHandler() {
+		return new SimpleUrlAuthenticationFailureHandler();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -26,6 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					.and()
 				.and()
 			.formLogin()
+				.failureHandler(failureHandler())
+				.and()
+			.csrf().disable()
+
 		;
 		// @formatter:on
 	}
@@ -39,5 +51,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.roles("USER")
 				.build()
 		);
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return token -> {
+			throw new UnsupportedOperationException("You must replace the `jwtDecoder` bean");
+		};
 	}
 }
---
+++ samples/bearer-token-filter-chain/src/test/java/org/springframework/security/community/samples/SpyOnTheFailureHandlerTests.java
@@ -0,0 +1,107 @@
+package org.springframework.security.community.samples;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("we can spy on failure handler")
+public class SpyOnTheFailureHandlerTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@SpyBean
+	AuthenticationFailureHandler failureHandler;
+
+	@SpyBean
+	public UserDetailsService userDetailsService;
+
+	private User.UserBuilder user = User.withDefaultPasswordEncoder()
+		.username("user")
+		.password("password")
+		.roles("USER");
+
+	@Test
+	public void accountExpired() throws Exception {
+		doReturn(user
+			.username("expired")
+			.accountExpired(true)
+			.build()
+		).when(userDetailsService).loadUserByUsername(any(String.class));
+		mvc.perform(
+			MockMvcRequestBuilders.post("/login")
+				.param("username", "expired")
+				.param("password", "password")
+		)
+			.andExpect(status().is4xxClientError())
+			.andExpect(unauthenticated())
+		;
+		Mockito.verify(failureHandler).onAuthenticationFailure(
+			any(),
+			any(),
+			any(AccountExpiredException.class)
+		);
+	}
+
+	@Test
+	public void accountLocked() throws Exception {
+		doReturn(user
+			.username("locked")
+			.accountLocked(true)
+			.build()
+		).when(userDetailsService).loadUserByUsername(any(String.class));
+		mvc.perform(
+			MockMvcRequestBuilders.post("/login")
+				.param("username", "locked")
+				.param("password", "password")
+		)
+			.andExpect(status().is4xxClientError())
+			.andExpect(unauthenticated())
+		;
+		Mockito.verify(failureHandler).onAuthenticationFailure(
+			any(),
+			any(),
+			any(LockedException.class)
+		);
+	}
+
+	@Test
+	public void happyPath() throws Exception {
+		doReturn(user.build())
+			.when(userDetailsService).loadUserByUsername(any(String.class));
+		mvc.perform(
+			MockMvcRequestBuilders.post("/login")
+				.param("username", "user")
+				.param("password", "password")
+		)
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("/"))
+			.andExpect(authenticated())
+		;
+	}
+
+}
