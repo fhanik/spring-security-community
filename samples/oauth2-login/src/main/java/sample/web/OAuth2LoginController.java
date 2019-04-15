d34e3715d3d8f9d4959a937a95220bd4c8d0c0b8
Filip Hanik <fhanik@pivotal.io>
2019-04-15 13:14:38 -0700

    Fixed the authorized client repository

--- samples/oauth2-login/src/main/java/sample/web/OAuth2LoginController.java
+++ samples/oauth2-login/src/main/java/sample/web/OAuth2LoginController.java
@@ -16,6 +16,8 @@
 package sample.web;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,10 +32,10 @@ public class OAuth2LoginController {
 
 	@GetMapping("/")
 	public String index(Model model,
-//						@RegisteredOAuth2AuthorizedClient("uaa") OAuth2AuthorizedClient authorizedClient,
+						@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
 						@AuthenticationPrincipal OAuth2User oauth2User) {
 		model.addAttribute("userName", oauth2User.getName());
-//		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
 		model.addAttribute("userAttributes", oauth2User.getAttributes());
 		return "index";
 	}
--- samples/oauth2-login/src/main/java/sample/web/SecurityConfig.java
+++ samples/oauth2-login/src/main/java/sample/web/SecurityConfig.java
@@ -1,12 +1,23 @@
 package sample.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebSecurity
+@EnableWebMvc
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public OAuth2AuthorizedClientRepository authorizedClientRepository(OAuth2AuthorizedClientService authorizedClientService) {
+		return new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(authorizedClientService);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
